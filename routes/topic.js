c4d5d9034cc1cd609eb34c09a19a8e8c7d53303b
egoing <egoing@gmail.com>
2018-08-12 22:56:00 +0900

    7.접근제어 - 글쓰기

--- routes/topic.js
+++ routes/topic.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 var auth = require('../lib/auth');
+var db = require('../lib/db');
+var shortid = require('shortid');
 
 router.get('/create', function (request, response) {
   if (!auth.isOwner(request, response)) {
@@ -35,9 +37,14 @@ router.post('/create_process', function (request, response) {
   var post = request.body;
   var title = post.title;
   var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-    response.redirect(`/topic/${title}`);
-  });
+  var id = shortid.generate();
+  db.get('topics').push({
+    id: id,
+    title: title,
+    description: description,
+    user_id: request.user.id
+  }).write();
+  response.redirect(`/topic/${id}`);
 });
 
 router.get('/update/:pageId', function (request, response) {
@@ -99,29 +106,31 @@ router.post('/delete_process', function (request, response) {
 });
 
 router.get('/:pageId', function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
+  var topic = db.get('topics').find({
+    id: request.params.pageId
+  }).value();
+  var user = db.get('users').find({
+    id:topic.user_id
+  }).value();
+  var sanitizedTitle = sanitizeHtml(topic.title);
+  var sanitizedDescription = sanitizeHtml(topic.description, {
+    allowedTags: ['h1']
+  });
+  var list = template.list(request.list);
+  var html = template.HTML(sanitizedTitle, list,
+    `
+    <h2>${sanitizedTitle}</h2>
+    ${sanitizedDescription}
+    <p>by ${user.displayName}</p>
+    `,
+    ` <a href="/topic/create">create</a>
             <a href="/topic/update/${sanitizedTitle}">update</a>
             <form action="/topic/delete_process" method="post">
               <input type="hidden" name="id" value="${sanitizedTitle}">
               <input type="submit" value="delete">
             </form>`,
-        auth.statusUI(request, response)
-      );
-      response.send(html);
-    }
-  });
+    auth.statusUI(request, response)
+  );
+  response.send(html);
 });
 module.exports = router;
