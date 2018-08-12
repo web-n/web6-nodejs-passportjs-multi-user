4af4800067a6bce075e2f1fc5f2890a8944ffb31
egoing <egoing@gmail.com>
2018-08-12 17:15:45 +0900

    회원가입 UI 구현

--- routes/auth.js
+++ routes/auth.js
@@ -38,6 +38,29 @@ module.exports = function (passport) {
       successFlash: true
     }));
 
+  router.get('/register', function (request, response) {
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    var title = 'WEB - login';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+        <div style="color:red;">${feedback}</div>
+        <form action="/auth/register_process" method="post">
+          <p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="password" name="pwd" placeholder="password"></p>
+          <p><input type="password" name="pwd2" placeholder="password"></p>
+          <p><input type="text" name="displayName" placeholder="display name"></p>
+          <p>
+            <input type="submit" value="register">
+          </p>
+        </form>
+      `, '');
+    response.send(html);
+  });
+
   router.get('/logout', function (request, response) {
     request.logout();
     request.session.save(function () {
