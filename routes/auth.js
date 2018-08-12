07d39c71fe3029a0fd4c9a435b8e064cfe221353
egoing <egoing@gmail.com>
2018-08-12 20:27:38 +0900

    4.회원정보저장

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,11 @@ var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 
 
-
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+db.defaults({users:[]}).write();
 
 module.exports = function (passport) {
   router.get('/login', function (request, response) {
@@ -49,16 +53,30 @@ module.exports = function (passport) {
     var html = template.HTML(title, list, `
         <div style="color:red;">${feedback}</div>
         <form action="/auth/register_process" method="post">
-          <p><input type="text" name="email" placeholder="email"></p>
-          <p><input type="password" name="pwd" placeholder="password"></p>
-          <p><input type="password" name="pwd2" placeholder="password"></p>
-          <p><input type="text" name="displayName" placeholder="display name"></p>
+          <p><input type="text" name="email" placeholder="email" value="egoing7777@gmail.com"></p>
+          <p><input type="password" name="pwd" placeholder="password" value="111111"></p>
+          <p><input type="password" name="pwd2" placeholder="password" value="111111"></p>
+          <p><input type="text" name="displayName" placeholder="display name" value="egoing"></p>
           <p>
             <input type="submit" value="register">
           </p>
         </form>
       `, '');
     response.send(html);
+  });
+
+  router.post('/register_process', function (request, response) {
+    var post = request.body;
+    var email = post.email;
+    var pwd = post.pwd;
+    var pwd2 = post.pwd2;
+    var displayName = post.displayName;
+    db.get('users').push({
+      email:email,
+      password:pwd,
+      displayName:displayName
+    }).write();
+    response.redirect('/');
   });
 
   router.get('/logout', function (request, response) {
